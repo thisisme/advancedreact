cf4799a918f321abb9d3b540e5fdb66706d4e1fb
Mats Olsberg <mats.olsberg@evry.com>
2021-02-16 09:01:20 +0100

    Add pages and components

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,16 +1,49 @@
 import Link from 'next/link';
+import styled from 'styled-components';
 import Nav from './Nav';
+
+const Logo = styled.h1`
+  background: red;
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  transform: skew(-7deg);
+  z-index: 2;
+  a {
+    color: white;
+    padding: 0.5rem 1rem;
+    text-decoration: none;
+    text-transform: uppercase;
+  }
+`;
+
+const HeaderStyles = styled.header`
+  .bar {
+    align-items: center;
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto;
+    justify-content: space-between;
+  }
+  .sub-bar {
+    border-bottom: 1px solid var(--black, black);
+    display: grid;
+    grid-template-columns: 1fr auto;
+  }
+`;
 
 export default function Header() {
   return (
-    <header>
+    <HeaderStyles>
       <div className="bar">
-        <Link href="/">Sick fits</Link>
+        <Logo>
+          <Link href="/">Sick fits</Link>
+        </Logo>
       </div>
       <div className="sub-bar">
-        <p />
+        <p>Search</p>
       </div>
       <Nav />
-    </header>
+    </HeaderStyles>
   );
 }
